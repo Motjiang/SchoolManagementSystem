2000e9e91b01c0a5d68609dc235034834d5d2f5b
Motjiang <motjiangbrilliant@gmail.com>
2024-07-26 17:16:30 +0200

    The view details is working

--- Controllers/StudentsController.cs
+++ Controllers/StudentsController.cs
@@ -62,6 +62,28 @@ namespace SchoolManagementSystem.Controllers
                 }
             }
         }
+        //Details
+        public async Task<IActionResult> Details(int id)
+        {
+            Student studentDetails = new Student();
+            using (var _httpClient = new HttpClient())
+            {
+                _httpClient.BaseAddress = new Uri(baseUrl + $"api/Students/{id}");
+                _httpClient.DefaultRequestHeaders.Accept.Clear();
+                _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
+                HttpResponseMessage response = await _httpClient.GetAsync("");
+                if (response.IsSuccessStatusCode)
+                {
+                    string result = response.Content.ReadAsStringAsync().Result;
+                    studentDetails = JsonConvert.DeserializeObject<Student>(result);
+                }
+                else
+                {
+                    return View("ErrorPage");
+                }
+                return View(studentDetails);
+            }
+        }
     }
 }
