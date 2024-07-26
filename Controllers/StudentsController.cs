ad2cce7707b8a11057a2e7ee415404e643d4301b
Motjiang <motjiangbrilliant@gmail.com>
2024-07-26 15:09:27 +0200

    students controller

--- Controllers/StudentsController.cs
+++ Controllers/StudentsController.cs
@@ -1,11 +1,41 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SchoolManagementSystem.Models;
+using System.Net.Http.Headers;
 
 namespace SchoolManagementSystem.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class StudentsController : ControllerBase
+    public class StudentsController : Controller
     {
+        private string baseUrl = "https://localhost:7008/";
+
+        public async Task<IActionResult> Index()
+        {
+            List<Student> lststudents = new List<Student>();
+            using (var _httpClient = new HttpClient())
+            {
+                _httpClient.BaseAddress = new Uri(baseUrl + "api/Students");
+                _httpClient.DefaultRequestHeaders.Accept.Clear();
+                _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage response = await _httpClient.GetAsync("");
+                if (response.IsSuccessStatusCode)
+                {
+                    string result = response.Content.ReadAsStringAsync().Result;
+                    lststudents = JsonConvert.DeserializeObject<List<Student>>(result);
+                }
+                else
+                {
+                    return View("ErrorPage");
+                }
+            }
+            return View();
+        }
+        //error page
+        public IActionResult ErrorPage()
+        {
+            return View();
+        }
+
     }
 }
